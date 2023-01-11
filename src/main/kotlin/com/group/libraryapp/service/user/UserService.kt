0095c7b6c7e8d9939f82fb507bed442fbba99110
hyun6ik <fkanwm@naver.com>
2023-01-12 08:41:35 +0900

    feat : 서비스 계층을 Kotlin으로 변경하기 - UserService.java

---
+++ src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -0,0 +1,47 @@
+package com.group.libraryapp.service.user
+
+import com.group.libraryapp.domain.user.User
+import com.group.libraryapp.domain.user.UserRepository
+import com.group.libraryapp.dto.user.request.UserCreateRequest
+import com.group.libraryapp.dto.user.request.UserUpdateRequest
+import com.group.libraryapp.dto.user.response.UserResponse
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class UserService(
+    private val userRepository: UserRepository,
+) {
+
+    @Transactional
+    fun saveUser(request: UserCreateRequest) {
+        val initUser = with(request) {
+            User(
+                name = name,
+                age = age,
+            )
+        }
+        userRepository.save(initUser)
+    }
+
+    @Transactional(readOnly = true)
+    fun getUsers(): List<UserResponse> {
+        return userRepository.findAll()
+            .map { user -> UserResponse(user) }
+            .toList()
+    }
+
+    @Transactional
+    fun updateUserName(request: UserUpdateRequest) {
+        val user = userRepository.findById(request.id)
+            .orElseThrow { IllegalArgumentException() }
+        user.updateName(request.name)
+    }
+
+    @Transactional
+    fun deleteUser(name: String) {
+        val user = userRepository.findByName(name)
+            .orElseThrow { IllegalArgumentException() }
+        userRepository.delete(user)
+    }
+}
